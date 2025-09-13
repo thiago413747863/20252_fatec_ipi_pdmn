9c336a900373b6fe3a1ad06366d30625a8b96e2e
Thiago Santos <thiago.santana8@fatec.sp.gov.br>
2025-09-12 21:54:46 -0300

    learn(ap03-react): encerra a apostila

---
+++ intro_git/ap03-react-componentes-jsx/src/App.jsx
@@ -0,0 +1,35 @@
+import './styles.css'
+const App = () => {
+
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+
+  const textoDoRotulo = 'Nome:' 
+
+  const obterTextoDoBotao = () => {
+    return "Enviar"
+  }
+
+  const aoClicar = () => {
+    alert('oi')
+  }
+
+    return <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+    <label
+      className="rotulo" 
+      htmlFor="nome" 
+      style={{display: 'block', marginBottom: 4}}>
+        {textoDoRotulo}
+    </label>
+    <input 
+      type="text" 
+      id="nome"
+      style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none' }}/>
+      <button 
+        style={estilosBotao}
+        onClick={() => aoClicar()}>
+        {obterTextoDoBotao()}
+      </button> 
+  </div>
+}
+
+export default App
